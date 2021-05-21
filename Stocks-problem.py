16d3dcc6c27961b1b89d87bc85763db4674c3a0c
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2021-05-21 02:32:43 -0500

    Update Stocks-problem.py

--- Stocks-problem.py
+++ Stocks-problem.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
+# First solved it using brute force method of looping through two arrays and keeping track of max difference, It takes O(n) time
+# Using Dynamic programming concept and just through one traversion we can find max value by keeping track of them
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         minimum = float('inf');
