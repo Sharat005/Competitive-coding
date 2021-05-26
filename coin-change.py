a8d5a1d1e999c3b6d453110aa92d5aa19cd080c1
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2021-05-26 02:52:14 -0500

    Coin change Problem from Leetcode

---
+++ coin-change.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        # Starting approach used at solving the problem, -1 case is not covered. Basic testcases passed
+
+        # coinsSorted = sorted(coins, reverse=True);
+        # count = -1;
+        # for i in range(len(coinsSorted)+1):
+        #     if(amount==0):
+        #         return count + 1;
+        #         break;
+        #     elif(amount<coinsSorted[i]):
+        #         continue;
+        #     else:
+        #         count = count + (amount/coinsSorted[i]);
+        #         amount = amount - (coinsSorted[i] * (amount/coinsSorted[i]))
+        
+        # Final approach which passed all the test cases
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for c in coins:
+            for x in range(c, amount + 1):
+                dp[x] = min(dp[x], dp[x - c] + 1);
+                
+        if(dp[amount] != float('inf')):
+            return dp[amount];
+        else:
+            return -1;
+        
