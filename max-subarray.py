16e16e2a510c294f73a1a6985b691bed3d9d8d14
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2021-08-11 00:27:05 -0500

    Create max-subarray.py

---
+++ max-subarray.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/maximum-subarray/submissions/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        if(len(nums)==1):
+            return nums[0];
+         
+        n = len(nums);
+        dp = [0] * n;
+        dp[0] = nums[0];
+        
+        for i in range(1, len(nums)):
+            dp[i] = max(nums[i], dp[i-1]+nums[i]);
+            
+        return max(dp);
+        
+        
