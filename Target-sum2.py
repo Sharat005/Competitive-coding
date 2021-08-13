bc5ac3b1ec263352657617494b39264d3f4f968a
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2021-08-13 00:58:15 -0500

    Hashmap approach to Target sum problem

---
+++ Target-sum2.py
@@ -0,0 +1,18 @@
+# This is the second approach to solve target_sum problem by using Hashmap aka dictionary in python
+
+def pair_with_targetsum(arr, target_sum):
+  nums = {}  # to store numbers and their indices
+  for i, num in enumerate(arr):
+    if target_sum - num in nums:
+      return [nums[target_sum - num], i]
+    else:
+      nums[arr[i]] = i
+  return [-1, -1]
+
+
+def main():
+  print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+  print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
