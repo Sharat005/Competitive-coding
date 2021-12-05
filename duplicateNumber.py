58aeda7747101e161f0f3d5aeee9c709a58a5e00
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2021-12-05 06:17:03 -0800

    Update duplicateNumber.py

--- duplicateNumber.py
+++ duplicateNumber.py
@@ -3,6 +3,16 @@
 # At first used brute force but the search takes a lot of time om top of a loop
 # Later used concept of sets from python which makes sure there are no duplicate elements in it
 
+# First basic way using loops
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        sortedNums = sorted(nums)
+        for i in range(len(sortedNums)-1):
+            if(sortedNums[i]==sortedNums[i+1]):
+              return True
+            elif(i==len(sortedNums)):
+              return False
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         print(set(nums))
