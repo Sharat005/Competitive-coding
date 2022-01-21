3bd496e60ff7e578c1ee611bdac22b2cfeb7803c
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-01-21 09:36:40 -0600

    Update Stocks-problem.py

--- Stocks-problem.py
+++ Stocks-problem.py
@@ -13,5 +13,23 @@ class Solution:
             elif(prices[i]-minimum > maximum):
                 maximum = prices[i]-minimum;
         return maximum;
+    
+ # Solving it for second time without use of maximum and using DP method
+ class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        min = prices[0]
+        dp = [0] * len(prices)
+        
+        for i in range(1, len(prices)):
+            if(prices[i] < min):
+                min = prices[i]
+            else:
+                dp[i] = prices[i] - min
+                
+        return max(dp)
+            
+            
+            
             
         
