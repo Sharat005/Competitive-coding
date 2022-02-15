8e624bcbdff97ac2bebdfef69ca99522525bc55b
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-02-14 20:38:29 -0600

    Time: 44 ms (99.51%), Space: 13.9 MB (93.87%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if(len(s) > 1000 or numRows > 1000):
+            return None
+        output = [""]* (min(numRows, len(s)))
+                
+        down = False
+        row = 0
+        
+        if(numRows is 1):
+            return s
+        
+        for c in range(len(s)):
+            output[row] += s[c]
+            if(row == 0 or row == numRows - 1):
+                down = not down
+            if(down):
+                row += 1
+            else:
+                row -= 1
+                
+        s1 = "".join(output)
+        
+        return s1
