57af0aea68c2e5e66fef9d00ba08348207dcc935
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-02-17 14:34:11 -0600

    Time: 109 ms (9.58%), Space: 14 MB (79.20%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        
+        dict = {}
+        output = []
+        
+        for domain in cpdomains:
+            arr = domain.split(' ')
+            addr = arr[1]
+            while (addr):
+                if addr not in dict:
+                    dict[addr] = 0
+                dict[addr] += int(arr[0])
+                addr = ".".join(addr.split('.')[1:])
+                print(addr)
+            
+        print(dict)
+        for key, value in dict.items():
+            output.append(str(value) + " " + key)
+        return output
