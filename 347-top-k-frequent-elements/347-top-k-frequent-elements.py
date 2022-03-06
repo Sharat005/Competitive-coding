eebfd0a86e49d51ef2de26fe004376dc82ae16cd
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-03-06 13:37:44 -0600

    Time: 155 ms (43.50%), Space: 18.6 MB (74.25%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,25 +1,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        dictt = {}
-        output = []
-        counter = 0
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
+            
         
-        for i in range(len(nums)):
-            if nums[i] in dictt:
-                dictt[nums[i]] += 1
-            else:
-                dictt[nums[i]] = 1
-                
-        dictt = dict(sorted(dictt.items(), key=lambda item: item[1], reverse=True))     
-        print(dictt)
+#         dictt = {}
+#         output = []
+#         counter = 0
         
-        for key,val in dictt.items():
-            if counter < k:
-                output.append(key)
-            else:
-                break
-            counter += 1
+#         for i in range(len(nums)):
+#             if nums[i] in dictt:
+#                 dictt[nums[i]] += 1
+#             else:
+#                 dictt[nums[i]] = 1
                 
-        return output
+#         dictt = dict(sorted(dictt.items(), key=lambda item: item[1], reverse=True))     
+        
+#         for key,val in dictt.items():
+#             if counter < k:
+#                 output.append(key)
+#             else:
+#                 break
+#             counter += 1
                 
+#         return output
