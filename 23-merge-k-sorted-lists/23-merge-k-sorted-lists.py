6858c38dffc36f77cc427825ce7ae3ba896c6561
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-03-07 02:16:39 -0600

    Time: 291 ms (14.13%), Space: 18.9 MB (10.86%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,16 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+        
+#     def __lt__(self, other):    -  This func lets you add custom less than operation which is done durong minHeap process
+#         return self.value < other.value
+
+
+from queue import PriorityQueue
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
-        self.nodes = []
-        head = point = ListNode(0)
-        for l in lists:
-            while l:
-                self.nodes.append(l.val)
-                l = l.next
-        for x in sorted(self.nodes):
-            point.next = ListNode(x)
-            point = point.next
+        ListNode.__lt__=lambda self,y:self.val<=y.val
+        minHeap = PriorityQueue()
+        for root in lists:
+            if root is not None:
+                minHeap.put((root.val, root))
+                            
+        head = tail = ListNode(0)
+        
+        while not minHeap.empty():
+            val, node = minHeap.get()
+                
+            tail.next = ListNode(val)
+            tail = tail.next
+            node = node.next
+            
+            if node is not None:
+                minHeap.put((node.val, node))
+                
         return head.next
+            
+            
+        
+        # self.nodes = []
+        # head = point = ListNode(0)
+        # for l in lists:
+        #     while l:
+        #         self.nodes.append(l.val)
+        #         l = l.next
+        # for x in sorted(self.nodes):
+        #     point.next = ListNode(x)
+        #     point = point.next
+        # return head.next
