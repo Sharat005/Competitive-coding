21531bc0442f6d331afec8286a9c4da118db862c
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-03-08 23:23:54 -0600

    Time: 54 ms (62.47%), Space: 14.2 MB (74.67%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -39,32 +39,3 @@ class Solution:
                     right = mid - 1
             
         return -1
-        
-#         i, j = 0, len(nums) - 1
-        
-#         if(len(nums) == 1):
-#             if(nums[0] == target):
-#                 return 0
-#             else:
-#                 return -1
-                
-#         while i <= j:
-            
-#             mid = i + (j-i) // 2
-            
-#             if nums[mid] == target:
-#                 return mid
-            
-#             if nums[mid] > nums[i]:
-#                 if target >= nums[i] and target < nums[mid]:
-#                     j = mid - 1
-#                 else:
-#                     i = mid + 1
-                
-#             else:
-#                 if target > nums[mid] and target <= nums[j]:
-#                     i = mid + 1
-#                 else:
-#                     j = mid - 1
-                    
-#         return -1
