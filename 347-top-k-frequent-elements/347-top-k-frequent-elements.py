679ed0b4c2d360a8ecfb10b8e9e58eba42e1c250
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-03-09 04:54:29 -0600

    Time: 183 ms (22.71%), Space: 18.6 MB (73.56%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,8 +1,37 @@
+from queue import PriorityQueue
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        count = Counter(nums)
-        return heapq.nlargest(k, count.keys(), key=count.get)
+        if not nums:
+	        return []
+
+        if len(nums) == 1:
+             return nums
+
+        # first find freq - freq dict
+        d = {}
+        for num in nums:
+            if num in d:
+                d[num] -= 1 # reverse the sign on the freq for the heap's sake
+            else:
+                d[num] = -1
+
+        h = []
+        from heapq import heappush, heappop
+        for key in d:
+            heappush(h, (d[key], key))
+
+        res = []
+        count = 0
+        while count < k:
+            frq, item = heappop(h)
+            res.append(item)
+            count += 1
+        return res
+        
+        
+        # count = Counter(nums)
+        # return heapq.nlargest(k, count.keys(), key=count.get)
             
         
 #         dictt = {}
