124265bcf423fdff0380e2d7ccff0ff129f815fc
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-03-11 16:04:47 -0600

    Time: 672 ms, Memory: 16.5 MB - LeetHub

---
+++ number-of-islands/number-of-islands.py
@@ -0,0 +1,34 @@
+class Solution:
+    def dfs(self, grid, i, j):
+        nr = len(grid)
+        nc = len(grid[0])
+        
+        if i < 0 or j < 0 or i >= nr or j >= nc or grid[i][j] == '0':
+            return
+        
+        grid[i][j] = '0'
+        self.dfs(grid, i-1,j)
+        self.dfs(grid, i+1,j)
+        self.dfs(grid, i,j-1)
+        self.dfs(grid, i,j+1)
+        
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        if grid is None or len(grid) == 0:
+            return 0;
+        
+        nr = len(grid)
+        nc = len(grid[0])
+        count = 0
+        
+        for i in range(nr):
+            for j in range(nc):
+                if grid[i][j] == '1':
+                    count += 1
+                    self.dfs(grid, i, j)
+                    
+        return count
+    
+            
+            
+            
