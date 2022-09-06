f70102b423c5a7bf0699b889282e8db02c1c89b4
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-09-06 16:41:32 -0500

    Time: 312 ms (5.00%), Space: 17.4 MB (75.76%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,14 +1,61 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        anagramMap = {}
+        dictt = {}
+        output = []
         
-        for s in strs:
-            ss = str(sorted(s))
-            if ss not in anagramMap:
-                anagramMap[ss] = []
-            anagramMap[ss].append(s)
+        for ele in strs:
+            if str(sorted(ele)) not in dictt:
+                dictt[str(sorted(ele))] = []
+            dictt[str(sorted(ele))].append(ele)
+        
+        for key, val in dictt.items():
+            output.append(val)
             
-        return anagramMap.values()
+        return output
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         anagramMap = {}
+        
+#         for s in strs:
+#             ss = str(sorted(s))
+#             if ss not in anagramMap:
+#                 anagramMap[ss] = []
+#             anagramMap[ss].append(s)
+            
+#         return anagramMap.values()
             
         
