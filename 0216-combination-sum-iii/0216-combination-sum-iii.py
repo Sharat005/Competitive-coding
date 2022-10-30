388ad11d257b0a5e9f3020b42c2f97a1e5ac5752
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-10-30 13:53:32 -0500

    Time: 52 ms (69.24%), Space: 13.9 MB (29.52%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -2,43 +2,22 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         
         
-        results = []
-        def backtrack(remain, comb, next_start):
-            if remain == 0 and len(comb) == k:
-                # make a copy of current combination
-                # Otherwise the combination would be reverted in other branch of backtracking.
-                results.append(list(comb))
-                return
-            elif remain < 0 or len(comb) == k:
-                # exceed the scope, no need to explore further.
-                return
-
-            # Iterate through the reduced list of candidates.
-            for i in range(next_start, 9):
-                comb.append(i+1)
-                backtrack(remain-i-1, comb, i+1)
-                # backtrack the current choice
-                comb.pop()
-
-        backtrack(n, [], 0)
-
-        return results
-#         output = []
+        output = []
         
-#         def dfs(i, comb, total):
-#             if len(comb) == k and total == 0:
-#                 output.append(list(comb))
-#                 return
-#             elif total < 0 or len(comb) == k:
-#                 return
+        def dfs(start, comb, total):
+            if len(comb) == k and total == 0:
+                output.append(list(comb))
+                return
+            elif total < 0 or len(comb) == k:
+                return
             
-#             for i in range(i, n):
+            for i in range(start, 9):
                 
-#                 comb.append(i+1)
-#                 dfs(i+1, comb, total-i-1)
-#                 comb.pop()
+                comb.append(i+1)
+                dfs(i+1, comb, total-i-1)
+                comb.pop()
                 
-#         dfs(0, [], n)
+        dfs(0, [], n)
         
-#         return output
+        return output
             
