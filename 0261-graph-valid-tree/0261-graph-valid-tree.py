fa59d2886f286fb868d90853b44a8a27dd90a9c4
Sharat Naik <35519401+Sharat005@users.noreply.github.com>
2022-11-02 05:01:01 -0500

    Time: 168 ms (70.47%), Space: 15.5 MB (73.22%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,49 +1,30 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         
-    
-        if len(edges) != n - 1: return False
-
-        # Create an adjacency list.
-        adj_list = [[] for _ in range(n)]
-        for A, B in edges:
-            adj_list[A].append(B)
-            adj_list[B].append(A)
-
-        # We still need a seen set to prevent our code from infinite
-        # looping if there *is* cycles (and on the trivial cycles!)
-        seen = {0}
-        queue = collections.deque([0])
-
+        graph = {i: [] for i in range(n)}
+        
+        # Need to create an adjacencey list
+        for i in range(len(edges)):
+            firstVertice, secondVertice = edges[i]
+            
+            graph[secondVertice].append(firstVertice)
+            graph[firstVertice].append(secondVertice)
+        
+        queue = deque([0])
+        parent = {0: -1}
+        
         while queue:
-            node = queue.popleft()
-            for neighbour in adj_list[node]:
-                if neighbour in seen:
+            curr = queue.popleft()
+            
+            for i in graph[curr]:
+                if i == parent[curr]:
                     continue
-                seen.add(neighbour)
-                queue.append(neighbour)
-
-        return len(seen) == n
+                if i in parent:
+                    return False
                 
-#         graph = {i: [] for i in range(n)}
-        
-#         # Need to create an adjacencey list
-#         for i in range(len(edges)):
-#             firstVertice, secondVertice = edges[i]
-            
-#             graph[secondVertice].append(firstVertice)
-#             graph[firstVertice].append(secondVertice)
-        
-#         queue = deque([0])
-#         seen = {0}
-        
-#         while queue:
-#             curr = queue.popleft()
-            
-#             for i in graph[curr]:
+                parent[i] = curr
+                queue.append(i)
                 
-                
-#                 seen.add(i)
-#                 queue.append(i)
+        return len(parent) == n
 
             
